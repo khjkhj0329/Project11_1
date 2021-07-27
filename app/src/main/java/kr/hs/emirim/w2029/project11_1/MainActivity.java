564ea518a547e1fa2cbdf1249a730e63affed190
효정이 <w2029@e-mirim.hs.kr>
2021-07-27 10:25:11 +0900

    Edit GridAdapter.java(Show Dialog Event) Commit

--- app/src/main/java/kr/hs/emirim/w2029/project11_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/project11_1/MainActivity.java
@@ -16,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         grid1 = findViewById(R.id.grid1);
         GridAdapter adapter = new GridAdapter(this);
         grid1.setAdapter(adapter);
+
+
     }
 }
